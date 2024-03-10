ec0aecdf138e60041921cf3d70e7dc5d6e992806
jocelyn-soh <s.sohjoce@gmail.com>
2024-03-11 03:35:22 +0800

    Handle float overflow exception

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify 
+import math
 
 app = Flask(__name__)
 
@@ -11,12 +12,17 @@ def calculate():
     num1 = float(request.form['num1']) if request.form['num1'] else 0
     num2 = float(request.form['num2']) if request.form['num2'] else 0
     operation = request.form['operation']
+
     if operation == 'add':
         result = num1 + num2
     else: 
         result = num1 - num2
-
+        
+    if result >= 1e308:
+        return jsonify({'error': 'Result exceeds maximum representable value of 1e308'})
+        
     return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run()
+
